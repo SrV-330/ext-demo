1e3ed0716f936a3301990fcab9cd84fd747883dd
SrV <985693016@qq.com>
2022-05-09 00:06:50 +0800

    add chrome-ext

--- vue.config.js
+++ vue.config.js
@@ -1,66 +1,37 @@
- /*const {
-  defineConfig
-} = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    open: false,
-    port: 8090,
-  }
-}) */
-
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
 
-// 复制文件到指定目录
-const copyFiles = [{
-    from: path.resolve("src/plugins/manifest.3.json"),
-    to: `${path.resolve("dist")}/manifest.json`
-  },
-  {
-    from: path.resolve("src/assets"),
-    to: path.resolve("dist/assets")
-  }, {
-    from: path.resolve("src/plugins/inject.js"),
-    to: path.resolve("dist/js")
-  }
-];
+// Generate pages object
+const pagesObj = {};
 
-// 复制插件
-const plugins = [new CopyWebpackPlugin({
-  patterns: copyFiles
-})];
-
-// 页面文件
-const pages = {};
-// 配置 popup.html 页面
-const chromeName = ["popup"];
+const chromeName = ["popup", "options"];
 
 chromeName.forEach(name => {
-  pages[name] = {
-    entry: `src/${name}/main.js`,
-    template: `src/${name}/index.html`,
+  pagesObj[name] = {
+    entry: `src/${name}/index.js`,
+    template: "public/index.html",
     filename: `${name}.html`
   };
 });
 
+const plugins =
+  process.env.NODE_ENV === "production"
+    ? [
+        {
+          from: path.resolve("src/manifest.production.json"),
+          to: `${path.resolve("dist")}/manifest.json`
+        }
+      ]
+    : [
+        {
+          from: path.resolve("src/manifest.development.json"),
+          to: `${path.resolve("dist")}/manifest.json`
+        }
+      ];
+
 module.exports = {
-  pages,
-  productionSourceMap: false,
-  // 配置 content.js background.js
+  pages: pagesObj,
   configureWebpack: {
-    entry: {
-      background: "./src/background/main.js"
-    },
-    output: {
-      filename: "js/[name].js"
-    },
-    plugins
-  },
-  // 配置 content.css
-  css: {
-    extract: {
-      filename: "css/[name].css"
-    }
+    plugins: [new CopyWebpackPlugin({patterns:plugins})]
   }
-}
+};
